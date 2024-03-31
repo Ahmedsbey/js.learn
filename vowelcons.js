d4c22057d032608894a928c77e20f04869ee2150
Ahmedsbey <ahmedraz2533@gmail.com>
2024-03-31 13:47:39 +0100

    added

---
+++ vowelcons.js
@@ -0,0 +1,9 @@
+let vow = [1,2]
+let pgName = 'JavaScript Loops'
+
+function printvow(vow){
+    for (let index = 0; index < vow.length; index++) {
+        const ele = vow[index];
+        console.log('f')
+    }
+}
